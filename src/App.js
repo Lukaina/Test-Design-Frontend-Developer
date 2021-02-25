f88ef183c49689a5ff4d26b34882c23bf03627dc
Verónica Alcaraz <60724393+Lukaina@users.noreply.github.com>
2021-02-25 13:04:19 -0500

    Design sample

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import { Card, CardTitle, CardText, CardActions, CardMenu, FABButton, Icon } from 'react-mdl'
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
 function App() {
   return (
     <div>
-      <h1>Hi</h1>
+      <Card shadow={0} className="card-container">
+        <CardTitle className="card-title">
+          Helsinki.
+          <CardText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Mauris sagittis pellentesque lacus eleifend lacinia...
+          </CardText>
+        </CardTitle>
+        <CardActions className="card-actions" border>
+          <FABButton mini>
+            <Icon name="add" />
+          </FABButton>
+        </CardActions>
+        <CardMenu className="card-menu">
+          <ArrowBackIcon/>
+        </CardMenu>
+      </Card>
     </div>
   );
 }
